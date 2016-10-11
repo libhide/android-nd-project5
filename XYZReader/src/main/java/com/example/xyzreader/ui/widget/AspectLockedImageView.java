f8a59d2b7b47527ed24ed39a0522859f46c46e56
Ratik Sharma <Ratik96@gmail.com>
2016-10-12 04:13:46 +0530

    Fix font size on detail view + some housekeeping

--- XYZReader/src/main/java/com/example/xyzreader/ui/widget/AspectLockedImageView.java
+++ XYZReader/src/main/java/com/example/xyzreader/ui/widget/AspectLockedImageView.java
@@ -1,19 +1,30 @@
 package com.example.xyzreader.ui.widget;
 
-/**
- * Created by Ratik on 12/10/16.
+/*
+ * Copyright 2015.  Emin Yahyayev
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-
-import android.view.View;
 
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.util.AttributeSet;
-
+import android.view.View;
 import android.widget.ImageView;
 
 import com.example.xyzreader.R;
 
+// Reference: http://bit.ly/2e7xY2p
 public final class AspectLockedImageView extends ImageView {
 
     private float aspectRatio = 0;
